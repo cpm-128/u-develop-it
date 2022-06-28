e37af470f7a3737bb2a63db2407015137a2788f5
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-28 17:39:53 -0400

    api routes to get parties and delete

--- server.js
+++ server.js
@@ -122,6 +122,62 @@ app.post('/api/candidate', ({ body }, res) => {
     //MySQL autogenerates the id
 });
 
+// GET all data from the parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET parties data based on id
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// DELETE PARTIES and set null
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found.'
+            });
+        } else {
+            res.json({
+                message: 'Deleted.',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
 // Default response for any reuqests that are Not Found
 app.use((req, res) => {
     res.status(404).end();
