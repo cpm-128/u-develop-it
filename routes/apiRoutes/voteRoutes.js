462536349d2d20d067bc3949c9ecedb248572235
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-29 21:15:21 -0400

    api route to GET totals for votes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -28,5 +28,26 @@ router.post('/vote', ({ body }, res) => {
     });
 });
 
+// GET totals
+router.get('/votes', (req, res) => {
+    const sql = `
+        SELECT candidates.*, parties.name AS party_name,
+        COUNT(candidate_id) AS count FROM votes
+        LEFT JOIN candidates ON votes.candidate_id = candidates.id
+        LEFT JOIN parties ON candidates.party_id = parties.id
+        GROUP BY candidate_id ORDER BY count DESC;
+    `;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 // export this file so the index.js file can import it
 module.exports = router;
